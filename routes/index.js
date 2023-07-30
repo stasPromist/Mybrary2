3e512b5b34ef5f8b0398262c342081cf6938ec4e
StasPromist <ctacbars@gmail.com>
2023-07-30 19:29:52 +0300

    Initial setup

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+// const Book = require('../models/book')
+
+router.get('/',(req, res) => {
+    res.render('index')
+    // let books
+    // try {
+    //   books = await Book.find().sort({ createdAt: 'desc' }).limit(10).exec()
+    // } catch {
+    //   books = []
+    // }
+    // res.render('index', { books: books })
+  })
+  
+  module.exports = router
